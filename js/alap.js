715cc3a018242f5061a9e49a44a84fdca98e852d
Hosszú Ádám <hosszuadam2004@gmail.com>
2021-06-02 20:28:51 +0200

    utazás tervező program, elírások, hibák javítása

--- js/alap.js
+++ js/alap.js
@@ -5,25 +5,22 @@ function $(id) {
 }
 
 function init() {
-  $("elerhetosegszoveg").addEventListener("mouseover",link,false);
-  $("elerhetosegszoveg").addEventListener("mouseout",link2,false);
+  $("elerhetosegszoveg").addEventListener("mouseover", link, false);
+  $("elerhetosegszoveg").addEventListener("mouseout", link2, false);
 
-  $("elerhetosegszoveg2").addEventListener("mouseover",link3,false);
-  $("elerhetosegszoveg2").addEventListener("mouseout",link4,false);
+  $("elerhetosegszoveg2").addEventListener("mouseover", link3, false);
+  $("elerhetosegszoveg2").addEventListener("mouseout", link4, false);
 
-  $("elerhetosegszoveg3").addEventListener("mouseover",link5,false);
-  $("elerhetosegszoveg3").addEventListener("mouseout",link6,false);
+  $("elerhetosegszoveg3").addEventListener("mouseover", link5, false);
+  $("elerhetosegszoveg3").addEventListener("mouseout", link6, false);
 
-  $("elerhetosegszoveg4").addEventListener("mouseover",link7,false);
-  $("elerhetosegszoveg4").addEventListener("mouseout",link8,false);
+  $("elerhetosegszoveg4").addEventListener("mouseover", link7, false);
+  $("elerhetosegszoveg4").addEventListener("mouseout", link8, false);
 
-  $("elerhetosegszoveg5").addEventListener("mouseover",link9,false);
-  $("elerhetosegszoveg5").addEventListener("mouseout",link10,false);
+  $("elerhetosegszoveg5").addEventListener("mouseover", link9, false);
+  $("elerhetosegszoveg5").addEventListener("mouseout", link10, false);
 
-  $("gomba").addEventListener("click",foglalas,false);
-
-  
-  
+  // $("gomba").addEventListener("click", foglalas, false);
 }
 function megnyitas() {
   $("egy").style.display = "block";
@@ -31,50 +28,58 @@ function megnyitas() {
 function bezar() {
   $("egy").style.display = "none";
 }
+function helyszamolas() {
+  let a = $("indulas");
+  let indulas = a.options[a.selectedIndex].text;
+  let b = $("erkezes");
+  let erkezes = b.options[b.selectedIndex].text;
+  let result = indulas + " kikötőből ma nem indul hajó " + erkezes + " felé a veszélyhelyzet miatt.";
+  window.alert(result);
+}
 function link() {
-  $("elerhetosegszoveg").style.color= "#aadaff";
-  $("elerhetosegszoveg").style.fontSize= "25px";
+  $("elerhetosegszoveg").style.color = "#aadaff";
+  $("elerhetosegszoveg").style.fontSize = "25px";
 }
 function link2() {
-  $("elerhetosegszoveg").style.color= "white";
-  $("elerhetosegszoveg").style.fontSize= "100%";
+  $("elerhetosegszoveg").style.color = "white";
+  $("elerhetosegszoveg").style.fontSize = "100%";
 }
 
 function link3() {
-  $("elerhetosegszoveg2").style.color= "#aadaff";
-  $("elerhetosegszoveg2").style.fontSize= "25px";
+  $("elerhetosegszoveg2").style.color = "#aadaff";
+  $("elerhetosegszoveg2").style.fontSize = "25px";
 }
 function link4() {
-  $("elerhetosegszoveg2").style.color= "white";
-  $("elerhetosegszoveg2").style.fontSize= "100%";
+  $("elerhetosegszoveg2").style.color = "white";
+  $("elerhetosegszoveg2").style.fontSize = "100%";
 }
 
 function link5() {
-  $("elerhetosegszoveg3").style.color= "#aadaff";
-  $("elerhetosegszoveg3").style.fontSize= "25px";
+  $("elerhetosegszoveg3").style.color = "#aadaff";
+  $("elerhetosegszoveg3").style.fontSize = "25px";
 }
 function link6() {
-  $("elerhetosegszoveg3").style.color= "white";
-  $("elerhetosegszoveg3").style.fontSize= "100%";
+  $("elerhetosegszoveg3").style.color = "white";
+  $("elerhetosegszoveg3").style.fontSize = "100%";
 }
 
 function link7() {
-  $("elerhetosegszoveg4").style.color= "#aadaff";
-  $("elerhetosegszoveg4").style.fontSize= "25px";
+  $("elerhetosegszoveg4").style.color = "#aadaff";
+  $("elerhetosegszoveg4").style.fontSize = "25px";
 }
 function link8() {
-  $("elerhetosegszoveg4").style.color= "white";
-  $("elerhetosegszoveg4").style.fontSize= "100%";
+  $("elerhetosegszoveg4").style.color = "white";
+  $("elerhetosegszoveg4").style.fontSize = "100%";
 }
 
 function link9() {
-  $("elerhetosegszoveg5").style.color= "#aadaff";
-  $("elerhetosegszoveg5").style.fontSize= "25px";
+  $("elerhetosegszoveg5").style.color = "#aadaff";
+  $("elerhetosegszoveg5").style.fontSize = "25px";
 }
 function link10() {
-  $("elerhetosegszoveg5").style.color= "white";
-  $("elerhetosegszoveg5").style.fontSize= "100%";
+  $("elerhetosegszoveg5").style.color = "white";
+  $("elerhetosegszoveg5").style.fontSize = "100%";
 }
-function foglalas(){
-    $("atirando").innerHTML="XDDDDDDDDDDDDD";
+function foglalas() {
+  $("atirando").innerHTML = "XDDDDDDDDDDDDD";
 }
